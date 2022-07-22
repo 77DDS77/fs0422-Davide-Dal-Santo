f400120f751661840dddb9de392b4b7c0f052791
77DDS77 <107913330+77DDS77@users.noreply.github.com>
2022-07-22 17:06:18 +0200

    push leaderboard still not properly working

---
+++ m2/w1/d5/Memory exam/PROGETTO-start/js/memory.js
@@ -0,0 +1,266 @@
+let arrayAnimali = ['🐱', '🦉', '🐾', '🦁', '🦋', '🐛', '🐝', '🐬', '🦊', '🐨', '🐰', '🐯', '🐱', '🦉', '🐾', '🦁', '🦋', '🐛', '🐝', '🐬', '🦊', '🐨', '🐯', '🐰'];
+//libreria per icone
+//https://html-css-js.com/html/character-codes/
+
+
+let arrayComparison = [];
+
+let scores = localStorage.getItem('Scores') || '[]';
+
+scores = JSON.parse(scores);
+
+console.log(scores);
+
+let tbody = document.querySelector('#leaderboard tbody')
+
+let nicknameSet = document.querySelector('#nicknameSet');
+
+document.body.onload = startGame();
+
+// mi serviranno alcune variabili 1. interval 2. una agganciata alla classe find 
+// 3. una agganciata al'id modal 4. una agganciata alla classe timer
+
+var interval; //var globale fuori dalla funct per resettarla ad ogni avvio
+
+var iconsFind = document.getElementsByClassName('find'); //mi serve dopo
+
+var modal = document.querySelector('#modal');//win message DIV
+
+var timer = document.querySelector('.timer');//timer DIV
+
+
+
+//una funzione che serve a mescolare in modo random gli elementi dell'array che viene passato 
+// (l'array contiene le icone degli animali)
+function shuffle(a) {
+    var currentIndex = a.length;
+    var temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = a[currentIndex];
+        a[currentIndex] = a[randomIndex];
+        a[randomIndex] = temporaryValue;
+    }
+    return a;
+}
+// una funzione che rimuove la classe active e chiama la funzione startGame()
+
+// la funzione startGame che pulisce il timer, dichiara un array vuoto, mescola casualmente l'array degli animali
+// (var arrayShuffle = shuffle(arrayAnimali);), aggancia il contenitore con id griglia, 
+// pulisce tutti gli elementi che eventualmente contiene
+// poi fa ciclo per creare i 24 div child -> aggiunge la class e l'elemento dell'array in base all'indice progressivo
+// chiama la funzione timer e associa a tutti gli elementi (div) di classe icon l'evento click e le due funzioni definit sotto
+
+
+function startGame(){
+
+    //shuffle
+    var arrayShuffle = shuffle(arrayAnimali);
+
+    //resetto il timer con metodo clearInterval
+    clearInterval(interval);
+
+    //svuoto l'array di appoggio da eventuali game precedenti
+    arrayComparison = [];
+
+    //aggancio div id griglia
+    let griglia = document.querySelector('#griglia');
+    //pulisco il suo contenuto
+    
+    while(griglia.hasChildNodes()){
+        griglia.removeChild(griglia.firstChild);//vedere se funziona
+        
+    }
+
+    //creazione della griglia
+    //24 card quindi limite ciclo for = 24
+    for(let z = 0; z < 24; z++){ //justice for the 'i' variable
+        let cardContainer = document.createElement('div');
+        let card = document.createElement('div');
+        card.classList.add('icon');
+        card.innerHTML = arrayShuffle[z];//assegno ad ogni carta il valore z pescando dall'array shufflato
+
+        griglia.append(cardContainer);
+        cardContainer.append(card)
+    }
+    
+
+    //invoco la funzione timerStart per far partire il timer
+    timerStart();
+
+
+    //prendo tutte le icon create e ciclandole invoco displayicon() per mostrarle
+    //e popUpModal() quando tutte le coppie sonon state trovate
+    var singleCard = document.getElementsByClassName("icon");
+    var cardGroup = [...singleCard];
+  
+    for(singleCard of cardGroup){
+      singleCard.addEventListener("click", displayIcon);
+      singleCard.addEventListener("click", popUpModal);
+     
+    }
+
+    leaderboardGenerator();
+
+}
+
+// =============================================================================================
+
+let iconClick = document.querySelectorAll('.icon');
+
+for(let singleIcon of iconClick){
+   singleIcon.addEventListener('click', displayIcon); 
+}
+
+
+
+function displayIcon() {
+    var icon = document.getElementsByClassName("icon");
+    var icons = [...icon];
+
+    /*
+    var icon = document.getElementsByClassName("icon");
+    var icons = [...icon];
+    è uguale a 
+    var icons = document.getElementsByClassName("icon");
+    //var icons = [...icon];
+    è un operatore che serve per passare un array come argomento:
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax 
+    https://www.tutorialspoint.com/es6/es6_operators.htm (cerca spread nella pagina)
+    */
+
+    //mette/toglie la classe show
+    this.classList.toggle("show");
+    //aggiunge l'oggetto su cui ha cliccato all'array del confronto
+    arrayComparison.push(this);
+
+    var len = arrayComparison.length;
+    //se nel confronto ci sono due elementi
+    if (len === 2) {
+        //se sono uguali aggiunge la classe find
+        if (arrayComparison[0].innerHTML === arrayComparison[1].innerHTML) {
+            arrayComparison[0].classList.add("find", "disabled");
+            arrayComparison[1].classList.add("find", "disabled");
+            arrayComparison = [];
+        } else {
+            //altrimenti (ha sbagliato) aggiunge solo la classe disabled
+            icons.forEach(function(item) {
+                item.classList.add('disabled');
+            });
+            // con il timeout rimuove  la classe show per nasconderli
+            setTimeout(function() {
+                arrayComparison[0].classList.remove("show");
+                arrayComparison[1].classList.remove("show");
+                icons.forEach(function(item) {
+                    item.classList.remove('disabled');
+                    for (var i = 0; i < iconsFind.length; i++) {
+                        iconsFind[i].classList.add("disabled");
+                    }
+                });
+                arrayComparison = [];
+            }, 700);
+        }
+    }
+}
+
+//una funzione che viene mostrata alla fine quando sono tutte le risposte esatte
+
+function popUpModal(){
+    if(iconsFind.length == 24){
+        clearInterval(interval);
+        modal.classList.add('active');
+        document.getElementById('tempoTrascorso').innerHTML = 
+        timer.innerHTML;
+
+        //local storage try
+        nicknameSet.addEventListener('click', (e) => {
+            e.preventDefault();
+            let nickname = document.querySelector('#nickname');
+            console.log(nickname, nickname.value);
+            let newScore = new User(nickname.value , timer.innerHTML)
+            console.log(newScore);
+
+            set_Score('Scores', newScore);
+            modal.classList.remove('active');
+            startGame();
+        });
+        
+    }
+
+}
+//generatore leaderboard da local storage
+
+
+// una funzione che nasconde la modale alla fine e riavvia il gioco
+
+function playAgain(){
+        modal.classList.remove('active');
+        startGame();
+};
+
+/*
+Se non volessi usare l'Onclick dentro il tagHTML potrei usare lo script seguente:
+
+let playAgainBtn = document.querySelector('p .button')
+
+playAgainBtn.addEventListener('click', playAgain);
+
+*/
+
+// una funzione che calcola il tempo e aggiorna il contenitore sotto
+
+function timerStart(){
+    let sec = 0;
+    let min = 0;
+
+    //Setto il timer con metodo Set interval
+    //intervallo da 1000ms, ogni iterazioen aumentano i secondi, 
+    //logic check per avanzamento minuti
+    interval = setInterval(function(){
+        timer.innerHTML= `Timer: ${min} min e ${sec} sec`;
+
+        sec++;
+
+        if(sec == 60){
+            min++;
+            sec = 0;
+        }
+        if(min == 60){
+            min = 0;
+            window.alert('Achievement Unlocked! More than an hour for a Memory match!')
+        }
+    }, 1000)
+}
+
+
+
+//-----------------Local Storage test area
+function set_Score(key,value){
+ //localStorage.setItem("name of variable", "value to store");
+    scores.push(value)
+    localStorage.setItem(key, JSON.stringify(scores));
+}//End set_LocalStorage
+
+class User{
+    constructor(username, score){
+        this.username = username;
+        this.score = score;
+    }
+}
+
+        //leaderboard generator test
+
+
+function leaderboardGenerator(){
+    for(let score of scores){
+        let tr = document.createElement('tr');
+        for(let prop in score){
+            let td = document.createElement('td');
+            td.innerHTML = score[prop];
+            tr.append(td);
+        }
+        tbody.append(tr);
+    }
+}
