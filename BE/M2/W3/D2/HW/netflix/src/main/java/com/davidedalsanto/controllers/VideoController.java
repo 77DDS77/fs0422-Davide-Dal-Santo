fb782b28ab815e62e12411b5ba01b2f0d0f02e84
77DDS77 <107913330+77DDS77@users.noreply.github.com>
2022-11-08 16:55:29 +0100

    HW M2W3D2

---
+++ BE/M2/W3/D2/HW/netflix/src/main/java/com/davidedalsanto/controllers/VideoController.java
@@ -0,0 +1,54 @@
+package com.davidedalsanto.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.davidedalsanto.entities.Category;
+import com.davidedalsanto.entities.Video;
+import com.davidedalsanto.services.VideoService;
+
+@RestController
+@RequestMapping("/api/catalog")
+public class VideoController {
+
+	@Autowired
+	VideoService vs;
+	
+	@GetMapping("")
+	public ResponseEntity<Iterable<Video>> getCatalog(){
+		return new ResponseEntity<>(vs.searchAllVideos(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/films")
+	public ResponseEntity<Iterable<Video>> getFilms(){
+		return new ResponseEntity<>(vs.findFilms(vs.searchAllVideos()), HttpStatus.OK);
+	}
+	
+	@GetMapping("/series")
+	public ResponseEntity<Iterable<Video>> getSeries(){
+		return new ResponseEntity<>(vs.findSeries(vs.searchAllVideos()), HttpStatus.OK);
+	}
+	
+	@GetMapping("/films/title/{title}")
+	public ResponseEntity<Iterable<Video>> getFilmsByTitle(@PathVariable String title){
+		return new ResponseEntity<>(
+				vs.findByTitle(vs.findFilms(vs.searchAllVideos()), title), HttpStatus.OK);
+	}
+	
+	@GetMapping("/films/category/{cat}")
+	public ResponseEntity<Iterable<Video>> getFilmsByCategoria(@PathVariable Category cat){
+		return new ResponseEntity<>(
+				vs.findByCategory(vs.findFilms(vs.searchAllVideos()), cat), HttpStatus.OK);
+	}
+	
+	@GetMapping("/title-category/{cat}/{title}")
+	public ResponseEntity<Iterable<Video>> getFilmsByTitleAndCategoria(@PathVariable String title, @PathVariable Category cat){
+		return new ResponseEntity<>(
+				vs.findByTitleAndCategory(vs.searchAllVideos(), title, cat), HttpStatus.OK);
+	}
+}
